1f593820a7f61aca58f88a78b39cc8974585b7bf
frytoli <>
2021-11-02 16:45:27 -0500

    scaled airflow workers and changes to environment and airflow variable uses

--- airflow/dags/db/__init__.py
+++ airflow/dags/db/__init__.py
@@ -5,19 +5,13 @@ from pyArango import theExceptions
 from requests import exceptions
 import datetime
 import time
-import os
 
 class arangodb():
-	def __init__(self):
+	def __init__(self, DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME):
 		# Attempt to establish a connection
 		self.db = None
 		while not self.db:
 			try:
-				DB_HOST = os.getenv('DB_HOST')
-				DB_PORT = os.getenv('DB_PORT')
-				DB_USER = os.getenv('DB_USER')
-				DB_PASS = os.getenv('DB_PASS')
-				DB_NAME = os.getenv('DB_NAME')
 				self.db = Connection(
 					arangoURL='http://{}:{}'.format(DB_HOST, DB_PORT),
 					username=DB_USER,

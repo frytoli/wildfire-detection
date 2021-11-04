ddf32d690426761f3de2d38b2b817ace5038da5d
frytoli <>
2021-11-04 12:51:35 -0500

    debugged api detection endpoint

--- api/app.py
+++ api/app.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
-from typing import Optional
+from pydantic import BaseModel
 from fastapi import FastAPI
+from typing import Optional
 import redis
 import os
 
 app = FastAPI()
 
+r = redis.Redis(host=os.getenv('BACKEND_HOST'), port=os.getenv('BACKEND_PORT'))
+
+class Image(BaseModel):
+    image: str
+
 @app.get('/')
 def get_root():
     return {'Hello': 'World'}
 
-@app.put('/detect')
-def put_detect(image: bytes):
-    return {'image': image}
+@app.post('/detect/', status_code=200)
+def post_detect(image: Image):
+    r.publish('redis-detection-channel', image.image)
--- scripts/api_test.py
+++ scripts/api_test.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 from requests_html import HTMLSession
+import base64
 
 if __name__ == '__main__':
     session = HTMLSession()
     r = session.post(
-        'http://127.0.0.1:80/detect',
-        data = {
-            'image': open('test.jpg', 'wb').read()
+        'http://127.0.0.1:8000/detect',
+        json = {
+            'image': base64.b64encode(open('test_images/frame0.jpg', 'rb').read()).decode('utf-8')
         }
     )
     print(r)

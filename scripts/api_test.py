d6b332aee8648b162085e8f4032653ca90620dce
frytoli <>
2021-11-05 14:24:41 -0500

    requests_html to requests

--- scripts/api_test.py
+++ scripts/api_test.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
-from requests_html import HTMLSession
+import requests
 import base64
 
 if __name__ == '__main__':
-    session = HTMLSession()
-    r = session.post(
+    r = requests.post(
         'http://127.0.0.1:8000/detect',
         json = {
             'image': base64.b64encode(open('test_images/frame0.jpg', 'rb').read()).decode('utf-8')

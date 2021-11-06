3411cc8d3331fbd5e4385f45af387cbb6595f87c
frytoli <>
2021-11-06 11:17:59 -0500

    api port changed to 1905

--- scripts/api_test.py
+++ scripts/api_test.py
@@ -5,7 +5,7 @@ import base64
 
 if __name__ == '__main__':
 	r = requests.post(
-		'http://127.0.0.1:8000/detect',
+		'http://127.0.0.1:1905/detect',
 		json = {
 			'image': base64.b64encode(open('test_images/frame0.jpg', 'rb').read()).decode('utf-8')
 		}

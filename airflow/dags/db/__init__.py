58de3ef3053c74805edffec546f6232d2b69af0c
frytoli <>
2021-11-16 22:42:13 -0600

    convert arangodb to mongodb

--- airflow/dags/db/__init__.py
+++ airflow/dags/db/__init__.py
@@ -1,118 +1,78 @@
 #!/usr/bin/env python3
 
-from pyArango.connection import *
-from pyArango import theExceptions
+from pymongo import MongoClient
 from requests import exceptions
+from pymongo import exceptions
 import datetime
 import time
-import uuid
 
-class arangodb():
+class mongo():
 	def __init__(self, DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME):
 		# Attempt to establish a connection
 		self.db = None
 		while not self.db:
 			try:
-				self.db = Connection(
-					arangoURL='http://{}:{}'.format(DB_HOST, DB_PORT),
-					username=DB_USER,
-					password=DB_PASS
-				)[DB_NAME]
-			except (theExceptions.ConnectionError, exceptions.ConnectionError) as e:
+				client = MongoClient(f'mongodb://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}')
+				self.db = client[DB_NAME]
+			except (exceptions.ConnectionError, exceptions.ServerSelectionTimeoutError) as e:
 				print(f'[!] Failed to establish a connection: {e}\n  [-] Trying again in 5 seconds')
 				time.sleep(5)
 
 	def get_docs(self, collection, tweetid=0):
 		'''
-			Fetch documents from database with optional time range filtering
+			Fetch documents from database with optional tweet id range filtering
 		'''
-		bindVars = {'@collection': collection}
 		if tweetid:
-			starttime = (datetime.datetime.utcnow() - datetime.timedelta(seconds=secsdelta)).isoformat
-			aql = '''
-				FOR doc IN @@collection
-					FILTER doc.tweetid > @tweetid
-					RETURN doc
-			'''
-			bindVars['tweetid'] = tweetid
+			collection = self.db[collection]
+			return list(collection.find({'tweetid': {'$gt': tweetid}}))
 		else:
-			aql = '''
-				FOR doc IN @@collection
-					RETURN doc
-			'''
-		return list(self.db.AQLQuery(aql, bindVars=bindVars, rawResults=True))
+			collection = self.db[collection]
+			return list(collection.find({}))
 
 	def insert_new_tweet(self, tweetid, text):
 		'''
 			Insert a new tweet document into the collection and ignore previously-seen tweets
 		'''
-		bindVars = {
-			'tweetid': tweetid,
-			'text': text,
-			'timestamp': datetime.datetime.utcnow().isoformat()
-		}
-		aql = '''
-			UPSERT { id: @tweetid }
-			INSERT { id: @tweetid, text: @text, scrape_timestamp: @timestamp }
-			UPDATE { } IN tweets
-			OPTIONS { exclusive: true }
-			RETURN { doc: NEW, type: OLD ? 'update' : 'insert' }
-		'''
-		return self.db.AQLQuery(aql, bindVars=bindVars, rawResults=True)
+		collection = self.db['tweets']
+		result = collection.update_one(
+			{ 'tweetid': tweetid, 'text': text },
+			{ '$set': { 'tweetid': tweetid } },
+			upsert = True
+		)
+		return result.upserted_id # ID of new record or None if record already exists
 
 	def get_count(self, collection):
-		bindVars = {'@collection': collection}
-		aql = '''RETURN LENGTH(@@collection)'''
-		resp = self.db.AQLQuery(aql, bindVars=bindVars, rawResults=True)
-		if len(resp) > 0:
-			return int(resp[0])
-		else:
-			return 0
+		collection = self.db[collection]
+		return int(collection.count())
 
-	def insert_image(self):
-		# Generate a uuid for the image
-		id = str(uuid.uuid4())
-		bindVars = {
-			'doc': {
-				'id': id,
-				'status': 'recieved',
-				'recieved_timestamp': datetime.datetime.utcnow().isoformat(),
-				'processed_timestamp': None,
-				'processed_time': None,
-				'fire': None,
-				'image': None
-			}
+	def insert_detection(self, id):
+		doc = {
+			'id': id,
+			'status': 'recieved',
+			'recieved_timestamp': datetime.datetime.utcnow().isoformat(),
+			'processed_timestamp': None,
+			'processed_time': None,
+			'fire': None,
+			'image': None
 		}
-		aql = '''
-			INSERT @doc INTO images
-		'''
-		self.db.AQLQuery(aql, bindVars=bindVars)
-		return id
+		# Insert
+		collection = self.db['detections']
+		result = collection.insert_one(doc)
 
-	def update_image(self, id, ptimestamp, ptime, fire, image=None):
+	def update_detection(self, id, ptimestamp, ptime, fire, image=None):
+		new_values = {
+			'status': 'detected',
+			'processed_timestamp': ptimestamp.isoformat(),
+			'processed_time': ptime,
+			'fire': fire,
+			'image': None
+		}
 		if image:
-			bindVars = {
-				'id': id,
-				'processed_timestamp': ptimestamp.isoformat(),
-				'processed_time': ptime,
-				'fire': fire,
-				'image': image
-			}
-			aql = '''
-				FOR doc IN images
-					FILTER doc.id == @id
-					UPDATE doc WITH { 'processed_timestamp': @processed_timestamp, 'processed_time': @processed_time, 'fire': @fire, 'image': @image } IN images
-			'''
-		else:
-			bindVars = {
-				'id': id,
-				'processed_timestamp': ptimestamp.isoformat(),
-				'processed_time': ptime,
-				'fire': fire
-			}
-			aql = '''
-				FOR doc IN images
-					FILTER doc.id == @id
-					UPDATE doc WITH { 'processed_timestamp': @processed_timestamp, 'processed_time': @processed_time, 'fire': @fire } IN images
-			'''
-		return self.db.AQLQuery(aql, bindVars=bindVars, rawResults=True)
+			new_values['image'] = image
+		# Update
+		collection = self.db['detections']
+		result = collection.update_one(
+			{ 'id': id },
+			{ '$set': new_values }
+		)
+		return result.upserted_id

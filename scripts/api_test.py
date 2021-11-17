783fa2be9e7639d56123c129e13f7f88a85f5ae7
frytoli <>
2021-11-16 22:38:53 -0600

    added 'id' field to payload

--- scripts/api_test.py
+++ scripts/api_test.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+'''
+Send a test image to the Detection API
+Last updated: 2021-11-16
+'''
+
 import requests
 import base64
 
@@ -7,6 +12,7 @@ if __name__ == '__main__':
 	r = requests.post(
 		'http://127.0.0.1:1905/detect',
 		json = {
+			'id': 'test',
 			'image': base64.b64encode(open('test_images/frame0.jpg', 'rb').read()).decode('utf-8')
 		}
 	)

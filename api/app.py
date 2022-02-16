48ba8660ac316923a0cef1831c9682acc624f773
frytoli <>
2022-02-15 22:23:06 -0600

    new endpoint for region scraping

--- api/app.py
+++ api/app.py
@@ -14,7 +14,15 @@ class Image(BaseModel):
 	id: str
 	image: str
 
+class AW_Regions(BaseModel):
+	regions: list
+
 @app.post('/detect/', status_code=200)
 def post_detect(image: Image):
 	r.publish('redis-detection-channel', f'{image.id}   {image.image}')
 	return image.id
+
+@app.post('/scrape-aw-region/', status_code=200)
+def post_scrape_aw(regions: AW_Regions):
+	r.publish('redis-alertwildfire-channel', f'{','.join(regions.regions)}')
+	return True
